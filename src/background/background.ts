89e3dffc2cea3c80e0b2d9d64fc9382db87e5ce1
Alejandro Aranda <93715524+gatofeli@users.noreply.github.com>
2025-12-14 17:57:35 +0100

    chore: limita la extensión a una sola forma de abrirse (#10)
    - comenta la recepción de comandos en el service worker
    - actualiza las documentaciones poniendo que la funcionalidad de abrir en nueva pestaña está en desarrollo
    - modifica el manifest.json para cambiar los comandos

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,14 +1,13 @@
 import { openInNewTab } from "./events/openInNewTab";
 
-chrome.commands.onCommand.addListener((command, tab) => {
-  if (command === "openInSameTab" && tab?.id == undefined) {
-    //todo: openInSameTab(tab.id....)
-  } else {
-    openInNewTab();
-  }
-});
+// chrome.commands.onCommand.addListener((command, tab) => {
+//   if (command === "openInSameTab" && tab?.id == undefined) {
+//     //todo: ------------------------
+//   } else {
+//     openInNewTab();
+//   }
+// });
 
-//! Provisional
 chrome.action.onClicked.addListener(() => {
   openInNewTab();
 });
