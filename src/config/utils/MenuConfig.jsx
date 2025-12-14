8667f191270bbc11495fcb5d8bf00f9e1923fa75
Alex Aranda <alearandam1993@gmail.com>
2025-12-14 01:25:36 +0100

    refactor(config):actualiza el menú y corrige etiquetado

--- src/config/utils/MenuConfig.jsx
+++ src/config/utils/MenuConfig.jsx
@@ -4,40 +4,44 @@ export function MenuConfig({ children }) {
   return (
     <menu className={styles["wrapper"]}>
       <ul className={styles["list"]}>
-        <a
-          href="../provider/configProvider.html"
-          className={`${styles["element"]} ${children === 0 ? styles["current"] : ""}`}
-        >
-          Webs guardadas
-        </a>
-        <a
-          href="../theme/configTheme.html"
-          className={`${styles["element"]} ${children === 1 ? styles["current"] : ""}`}
-        >
-          Temas
-        </a>
-        <a
-          href="../reportBug/ConfigBug.html"
-          className={`${styles["element"]} ${children === 2 ? styles["current"] : ""}`}
-        >
-          Bugs
-        </a>
-        <a
-          href="../../doc/help.html"
-          target="_blank"
-          rel="noopener"
-          className={`${styles["element"]} ${children === 3 ? styles["current"] : ""}`}
-        >
-          help
-        </a>
-        <a
-          href="../../doc/info.html"
-          target="_blank"
-          rel="noopener"
-          className={`${styles["element"]} ${children === 4 ? styles["current"] : ""}`}
-        >
-          info
-        </a>
+        <li>
+          <a
+            href="../provider/configProvider.html"
+            className={`${styles["element"]} ${children === 0 ? styles["current"] : ""}`}
+          >
+            Webs guardadas
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="../theme/configTheme.html"
+            className={`${styles["element"]} ${children === 1 ? styles["current"] : ""}`}
+          >
+            Temas
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="../reportBug/configBug.html"
+            className={`${styles["element"]} ${children === 2 ? styles["current"] : ""}`}
+          >
+            Bugs y Sugerencias
+          </a>
+        </li>
+
+        <li>
+          <a href="../../doc/help.html" target="_blank" rel="noopener" className={styles["element"]}>
+            Ayuda
+          </a>
+        </li>
+
+        <li>
+          <a href="../../doc/info.html" target="_blank" rel="noopener" className={styles["element"]}>
+            información
+          </a>
+        </li>
       </ul>
     </menu>
   );
